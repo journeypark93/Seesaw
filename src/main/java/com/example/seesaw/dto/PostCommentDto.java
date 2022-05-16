64da40ce07dc6635c8a214889502c9396ae36b1a
DESKTOP-0LUN99P\parkm <parkmansoo@nate.com>
2022-05-16 20:07:13 +0900

    * feat: 고민글 상세보기 댓글 페이징~

---
+++ src/main/java/com/example/seesaw/dto/PostCommentDto.java
@@ -0,0 +1,33 @@
+package com.example.seesaw.dto;
+
+import com.example.seesaw.model.PostComment;
+import com.example.seesaw.model.TroubleComment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PostCommentDto {
+    private Long commentId;
+    private String nickname;
+    private String comment;
+    private String commentTime;
+    private Long CommentLikeCount;
+    List<ProfileListDto> ProfileImages;
+    private boolean commentLikeStatus;
+
+
+    public PostCommentDto(PostComment postComment) {
+        this.commentId = postComment.getId();
+        this.nickname = postComment.getNickname();
+        this.comment = postComment.getComment();
+    }
+
+}
